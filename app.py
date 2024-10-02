98d841e4abdba4054f27076634595097b5382cfd
Gauri-dsml23 <129536325+Gauri-dsml23@users.noreply.github.com>
2024-10-02 19:05:02 +0530

    APP CREATED AND SUCCESS
    I did it, no need of virtual environment.
    as per teacher said, my system environment giving local host page, as per code.

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.header("_Streamlit_is :Blue[cool] :sunglasses:")
+st.header("This is a header with a divider", divider="gray")
+st.header("These headers have rotating dividers", divider=True)
+st.header("One", divider="blue")
+st.header("Two", divider="red")
+st.header("Three", divider="orange")
+st.header("Four", divider="green")
